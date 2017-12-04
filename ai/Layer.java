e28542db09a05413bf83f445283099abc93c33f4
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2017-12-04 16:31:19 +0900

    Add files via upload

--- ai/Layer.java
+++ ai/Layer.java
@@ -6,21 +6,14 @@ public class Layer {
 
 	int input;
 	int node;
-
-	Matrix weight;
-	Matrix bias;
+	Matrix weight = null;
 
 	public Layer(int input, int node) {
-		// input은 입력 값의 개수, node는 이 레이어의 노드 개수
 		this.input = input;
 		this.node = node;
-		// 가중치 행렬을 만든다.
 		weight = new Matrix(initialize(new double[input][node]));
-		// bias 행렬을 만든다.
-		bias = new Matrix(initialize(new double[1][input]));
 	}
 
-	// 이건 그냥 무시해도 되는 함수. 배열을 전부 0으로 채운다.
 	public double[][] initialize(double[][] input) {
 		for (int i = 0; i < input.length; i++) {
 			for (int j = 0; j < input[i].length; j++) {
@@ -30,7 +23,6 @@ public class Layer {
 		return input;
 	}
 
-	// 어떤 배열에 size 크기 이내로 랜덤한 숫자를 더하거나 빼 주는 함수. Genetic Algorithm에 사용하는 것.
 	public static double[][] appendRandom(double[][] input, double size) {
 		for (int i = 0; i < input.length; i++) {
 			for (int j = 0; j < input[i].length; j++) {
@@ -40,31 +32,42 @@ public class Layer {
 		return input;
 	}
 
-	// 위의 appendRandom을 weight(가중치)와 bias에 적용해주는 함수
 	public void appendRandom(double size) {
 		weight = new Matrix(appendRandom(weight.getArray(), size));
-		bias = new Matrix(appendRandom(bias.getArray(), size));
 	}
-	// 어떤 배열에 size 크기 이내로 랜덤한 숫자를 더하거나 빼 주는 함수. Genetic Algorithm에 사용하는 것.
-		public static double[][] append(double[][] input, double[][] size) {
-			for (int i = 0; i < input.length; i++) {
-				for (int j = 0; j < input[i].length; j++) {
-					input[i][j] = input[i][j] + size[i][j];
-				}
-			}
-			return input;
-		}
 
-		// 위의 appendRandom을 weight(가중치)와 bias에 적용해주는 함수
-		public void append(double[][] size) {
-			weight = new Matrix(append(weight.getArray(), size));
+	public static double[][] append(double[][] input, double[][] size) {
+
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[i].length; j++) {
+				input[i][j] = input[i][j] + size[i][j];
+			}
 		}
-	// 시그모이드 함수.
+		return input;
+	}
+
+	public void append(double[][] size) {
+		weight = new Matrix(append(weight.getArray(), size));
+	}
+
+	public void append(Matrix m) {
+		weight = m;
+		int a = weight.getColumnDimension();
+		int b = weight.getRowDimension();
+		double[][] d = weight.getArray();
+		double[][] D = new double[a][b];
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < a; j++) {
+				D[j][i] = d[i][j];
+			}
+		}
+		weight = new Matrix(D);
+	}
+
 	public double sigmoid(double input) {
 		return 1 / (1 + Math.exp(-input));
 	}
 
-	// 배열의 각 원소에 시그모이드를 적용해주는 함수.
 	public double[][] sigmoid(double[][] input) {
 		for (int i = 0; i < input.length; i++) {
 			for (int j = 0; j < input[i].length; j++) {
@@ -74,42 +77,14 @@ public class Layer {
 		return input;
 	}
 
-	// 입력 배열을 받아서 행렬 연산을 하는 함수.
 	public double[][] calc(double[][] input) {
 		Matrix inputConv = new Matrix(input);
-
-		// 입력값에 bias를 더해서
-		inputConv = inputConv.plus(bias);
-		// 가중치와 행렬 곱을 해 준다.
 		inputConv = inputConv.times(weight);
-		// 계산 결과의 값을 배열로 얻어서
 		double[][] temp = inputConv.getArray();
-		// sigmoid를 통과시켜 반환한다.
 		return sigmoid(temp);
 	}
 
-	// Genetic Algorithm에 사용할, 이 레이어를 복사하는 함수.
-	public Layer copy() {
-		Layer output = new Layer(input, node);
-		output.bias = this.bias.copy();
-		output.weight = this.weight.copy();
-		return output;
-	}
-
-	// 이 아래로는 단순히 배열 출력하는 함수. 1도 안 중요함.
-	public static void print(double[][] input) {
-		for (double[] i : input) {
-			for (double j : i) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
-	}
-
-	public static void print(double[] input) {
-		for (double j : input) {
-			System.out.print(j + " ");
-		}
-		System.out.println();
+	public void setweight(Matrix m) {
+		weight = m;
 	}
 }
--- ai/Test.java
+++ ai/Test.java
@@ -1,27 +1,22 @@
 package ai;
 
+import Jama.Matrix;
+
 public class Test {
-	public static void main(String[] args) {
-		// 노드 개수 정의
-		int[] nodes = { 21, 8, 8 };
-		double[][] size = null ;
-		// 레이어셋 만들기
-		LayerSet layerSet = new LayerSet(nodes);
+	static int[] nodes = { 21, 8, 8 };
+	static double[] x = new double[21];
+	static double[] y = new double[8];
+	static LayerSet layerSet = new LayerSet(nodes);
 
-		// for (int k = 0; k < 10; k++) {
-		// 레이어셋 섞기
+	public static void propagation(LayerSet layerSet, double[][] size) {
+		
 		layerSet.size(size);
-		// 정의역과 치역 정의
-		double[] x = new double[21];
-		double[] y = new double[8];
 		int i;
-
-		// 정의역과 치역에 값 넣기
+		
 		for (i = 0; i < 21; i++) {
-			x[i] =(int)( Math.random()*5);
+			x[i] = (int) (Math.random() * 5);
 		}
 		y = layerSet.calc(x);
-
 		// 출력.
 		for (i = 0; i < 21; i++) {
 			System.out.print(x[i] + " ");
@@ -31,6 +26,64 @@ public class Test {
 			System.out.print(y[i] + " ");
 		}
 		System.out.println(" ");
+	}
 
+	public static void propagation(LayerSet layerSet, Matrix[] M) {
+		
+		layerSet.size(M);
+		int i;
+		y = layerSet.calc(x);
+		
+		for (i = 0; i < 21; i++) {
+			System.out.print(x[i] + " ");
+		}
+		System.out.println(" ");
+		for (i = 0; i < 8; i++) {
+			System.out.print(y[i] + " ");
+		}
+		System.out.println(" ");
+	}
+
+	public static void main(String[] args) {
+		
+		double[][] size = new double[21][8];
+		
+		for (int i = 0; i < 21; i++) {
+			x[i] = (int) (Math.random() * 5);
+		}
+		for (int i = 0; i < 21; i++) {
+			for (int j = 0; j < 8; j++) {
+				size[i][j] = Math.random() * 5;
+			}
+		}
+		
+		propagation(layerSet, size);
+		System.out.println("   ");
+		
+		double[][] x1 = new double[1][];
+		x1[0] = x;
+		double[][] y1 = new double[1][];
+		B_LayerSet l = new B_LayerSet(nodes);
+		y1[0] = x;
+		Learner L = new Learner(l, x1, y1);
+		L.scgLearn(10000);
+
+		for (double[] data : x1) {
+			for (double y : l.calc(data)) {
+				System.out.println(y);
+			}
+		}
+		
+		Matrix[] M = L.getweight();
+		System.out.println("   ");
+		layerSet.setweight(M);
+		
+		for (int i = 0; i < 21; i++) {
+			for (int j = 0; j < 8; j++) {
+				size[i][j] = 0;
+			}
+		}
+		
+		propagation(layerSet, M);
 	}
 }
