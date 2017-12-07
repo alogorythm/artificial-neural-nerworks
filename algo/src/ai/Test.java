643763e034ae2130ad874dcbffe51ca2e437ebe1
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2017-12-08 02:01:18 +0900

    Update Test.java

--- algo/src/ai/Test.java
+++ algo/src/ai/Test.java
@@ -1,8 +1,10 @@
-package ai;
+package algo;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import Jama.Matrix;
 
@@ -51,88 +53,7 @@ public class Test {
 		
 	}
 
-	public static void showing() {
-		
-		double[][] size = new double[19][8];
-		
-		for (int i = 0; i < 19; i++) {
-			x[i]=UI.data[i];
-		}
-		for (int i = 0; i < 19; i++) {
-			for (int j = 0; j < 8; j++) {
-				size[i][j] = Math.random() * 5;
-			}
-		}
-		
-		propagation(layerSet, size);
-		System.out.println("   ");
-		
-		double[][] x1 ={ { 3, 2, 3, 3, 3, 1, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-				{ 1, 2, 1, 3, 1, 3, 1, 2, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 2 },
-				{ 1, 1, 3, 3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1 },
-				{ 1, 1, 1, 1, 3, 2, 1, 1, 3, 2, 2, 3, 1, 1, 1, 2, 2, 1, 1 },
-				{ 1, 1, 1, 3, 1, 3, 1, 1, 3, 1, 1, 2, 1, 1, 3, 1, 1, 1, 1 },
-				{ 1, 1, 1, 1, 3, 3, 1, 1, 2, 1, 1, 3, 2, 2, 3, 1, 1, 1, 1 },
-				{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 1, 1, 1, 1, 1, 3, 1, 1 },
-				{ 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 2, 1, 1, 1, 1, 2, 2, 1, 1 } };
-		double[][] y1 = { { 1, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 },
-				{ 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0 },
-				{ 0, 0, 0, 0, 0, 0, 1, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1 } };
-		B_LayerSet l = new B_LayerSet(nodes);
-		Learner L = new Learner(l, x1, y1);
-		L.scgLearn(100000);
-
-		for (double[] data : x1) {
-			for (double y : l.calc(data)) {
-				System.out.println(y);
-			}
-		}
-		
-		Matrix[] M = L.getweight();
-		for (int i = 0; i < M.length; i++) {
-			for (int j = 0; j < M[i].getRowDimension(); j++) {
-				for(int k=0;k<M[i].getColumnDimension();k++) {
-					System.out.print(M[i].get(j, k)+" ");
-				}
-				System.out.println("");
-			}
-			System.out.println("");
-		}
-		try {
-			filewrite(M);
-		} catch (IOException e) {
-			// TODO 자동 생성된 catch 블록
-			e.printStackTrace();
-		}
-		try {
-			M=fileread();
-		} catch (IOException e) {
-			// TODO 자동 생성된 catch 블록
-			e.printStackTrace();
-		}
-		for (int i = 0; i < M.length; i++) {
-			for (int j = 0; j < M[i].getRowDimension(); j++) {
-				for(int k=0;k<M[i].getColumnDimension();k++) {
-					System.out.print(M[i].get(j, k)+" ");
-				}
-				System.out.println("");
-			}
-			System.out.println("");
-		}
-		System.out.println("   ");
-		layerSet.setweight(M);
-		
-		for (int i = 0; i < 19; i++) {
-			for (int j = 0; j < 8; j++) {
-				size[i][j] = 0;
-			}
-		}
-		
-		propagation(layerSet, M);
-		for(int  m=0;m<8;m++)
-		diagnosis.testresult[m]=y[m];
-		diagnosis Diagnosis = new diagnosis();
-	}
+	
 	public static Matrix[] reverse(Matrix[] m) {
 		Matrix[] M=new Matrix[m.length];
 		for(int i=0;i<m.length;i++) {
@@ -242,5 +163,145 @@ public class Test {
 		System.out.println("   ");
 
 	}
+	public static double[][] dataread(String S) throws IOException {
+		double[][] D = null;
+		FileReader fr1 = null;
+		FileReader fr2 = null;
+		try {
+			fr1 = new FileReader(S);
+			String s = "";
+			int c1=0,c2=0,c=0;
+			while (true) {
+				int i;
+				i = fr1.read();
+				if (i == 32) {
+					c1++;
+					s = "";
+					continue;
+				}
+				if (i == 13) {
+					c2++;
+					c=c1;
+					c1=0;
+					continue;
+				}
+				if(i==-1)
+					break;
+				
+			}
+			D=new double[c2][c];
+			fr1.close();
+			fr2 =new FileReader(S);
+			s = "";
+			c1=0;
+			c2=0;
+			
+			while (true) {
+				int i;
+				
+				i = fr2.read();
+				if (i == 32) {
+					double d = Double.parseDouble(s);
+					D[c2][c1]=d;
+					s = "";
+					continue;
+				}
+				if (i == 13) {
+					fr2.read();
+					s = "";
+					c1=0;
+					c2++;
+					if(c2==D.length)
+						break;
+					continue;
+				}
+				s = s + String.valueOf((char) i);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			fr2.close();
+		}
+		return D;
+	}
 
+	public static void datawrite(String S,double[][] d) throws IOException {
+
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(S);
+			for (int i = 0; i < d.length; i++) {
+				for (int j = 0; j < d[i].length; j++) {
+						fw.write( d[i][j]+ " ");
+				}
+				fw.write("\r\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			fw.close();
+		}
+		System.out.println("   ");
+	}
+	static double[][] AtoD(ArrayList<double[]> A){
+		double[][] d = new double[A.size()][A.get(0).length];
+		for(int i=0;i<A.size();i++) {
+			d[i]=A.get(i);
+		}
+		return d;
+	}
+	public static void showing() throws IOException {
+		Matrix[] M = null;
+		double[][] size = new double[19][8];
+		Scanner scan = new Scanner(System.in);
+		String command;	
+		for (int i = 0; i < 19; i++) {
+			x[i]=UI.data[i];
+		}
+		/*for (int i = 0; i < 19; i++) {
+			for (int j = 0; j < 8; j++) {
+				size[i][j] = Math.random() * 1;
+			}
+		}
+		
+		propagation(layerSet, size);
+		*/
+		M=fileread();
+		propagation(layerSet, M);
+		System.out.println("   ");
+		System.out.println("학습?");
+		command=scan.next();
+		if(command.equals("yes")) {
+			double[][] x1 = dataread("inputdata.txt");
+			double[][] y1 = dataread("outputdata.txt");
+			B_LayerSet l = new B_LayerSet(nodes);
+			Learner L = new Learner(l, x1, y1);
+			L.scgLearn(100000);
+
+			for (double[] data : x1) {
+				for (double y : l.calc(data)) {
+					System.out.println(y);
+				}
+			}
+		
+			M = L.getweight();
+		}
+		System.out.println("   ");
+		System.out.println("적용?");
+		command=scan.next();
+		if(command.equals("yes")) {
+			layerSet.setweight(M);
+		
+			propagation(layerSet, M);
+		}
+		System.out.println("저장?");
+		command=scan.next();
+		if(command.equals("yes")) {
+			filewrite(M);
+
+		}
+		for(int  m=0;m<8;m++)
+		diagnosis.testresult[m]=y[m];
+		diagnosis Diagnosis = new diagnosis();
+	}
 }
